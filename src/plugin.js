81914529223b16e8d37a127b10d45a53cd9a2ff5
Jon Surrell <sirreal@users.noreply.github.com>
2021-06-11 13:17:17 +0200

    Remvoe selfHash references

--- src/plugin.js
+++ src/plugin.js
@@ -122,7 +122,6 @@ const defaultGetIconType = (fileName) => {
  * @property {(filename: string) => boolean} [isAssetManifestIcon] a function to determine if a webpack asset should be included as an icon in the web app manifest. The function accepts a `filename` parameter and returns true or false.
  * @property {(filename: string) => Dimensions} [getIconSize] a function to determine the icon size of any asset that passes the check `isAssetManifestIcon()`. The function accepts a `fileName` parameter and returns an object `{ width, height }`.
  * @property {(filename: string) => string} [getIconType] a function to determine the type of any asset that passes the check `isAssetManifestIcon()`. The function accepts a `fileName` parameter and returns a string describing the mime type of the asset, ex. "image/png".
- * @property {boolean} [selfHash=true]
  *
  * @typedef Dimensions
  * @property {number} width
