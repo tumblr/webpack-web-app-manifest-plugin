cd967752d2fed402c531ed13bd1cb39302e262ad
Jon Surrell <sirreal@users.noreply.github.com>
2021-12-29 16:05:20 +0100

    Make isAssetManifestIcon optional

--- src/index.ts
+++ src/index.ts
@@ -95,7 +95,7 @@ export interface Config {
   /** An output path where the manifest file should be written. */
   destination: string;
   /** A function to determine if a webpack asset should be included as an icon in the web app manifest. The function accepts a `filename` parameter and returns true or false. */
-  isAssetManifestIcon: (filename: string) => boolean;
+  isAssetManifestIcon?: (filename: string) => boolean;
   /** A function to determine the icon size of any asset that passes the check `isAssetManifestIcon()`. The function accepts a `fileName` parameter and returns an object `{ width, height }`. */
   getIconSize?: (filename: string) => Dimensions;
   /** A function to determine the type of any asset that passes the check `isAssetManifestIcon()`. The function accepts a `fileName` parameter and returns a string describing the mime type of the asset, ex. "image/png". */
